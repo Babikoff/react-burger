4372135424a39ee3ae835db82bcf5d42d81196c9
Ivan Babikov <i.babikov@itransition.com>
2026-05-20 12:55:03 +0500

    Удалена не нужная константа baseApiUrl

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,3 @@
-const baseApiUrl = 'https://new-stellarburgers.education-services.ru';
-export const ingredientsApiUrl = baseApiUrl + '/api/ingredients';
-
 export const DndItemTypes = {
   Ingredient: 'ingredient',
   ConstructorItem: 'constructorItem',
