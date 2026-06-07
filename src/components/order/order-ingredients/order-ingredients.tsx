49bb75555ce5fef3120345c572d76e85306cf8f3
Ivan Babikov <i.babikov@itransition.com>
2026-06-07 18:13:49 +0500

    Доработана вёрстка макета карточки заказа

--- src/components/order/order-ingredients/order-ingredients.tsx
+++ src/components/order/order-ingredients/order-ingredients.tsx
@@ -7,15 +7,27 @@ import type { JSX } from 'react';
 import styles from './order-ingredients.module.css';
 
 function OrderIngredients(): JSX.Element {
-  const ingredients = useSelector(selectIngredientsData);
+  const ingredients = useSelector(selectIngredientsData).slice(0, 5);
+  const lastIndex = ingredients.length - 1;
+  const lastItemText = '+2';
   return (
     <section className={styles.ingredients_line}>
-      {ingredients.slice(0, 5).map((ingredient) => (
-        <img
+      {ingredients.map((ingredient, index) => (
+        <div
           key={ingredient._id}
-          src={ingredient.image_mobile}
-          className={styles.ingredient_image}
-        />
+          className={styles.ingredient_circle}
+          style={{
+            left: `${-20 * index}px`,
+            zIndex: 6 - index,
+          }}
+        >
+          <img src={ingredient.image_mobile} className={styles.ingredient_image} />
+          {index === lastIndex && !!lastItemText && (
+            <span className={`${styles.last_image_label} text text_type_digits-default`}>
+              {lastItemText}
+            </span>
+          )}
+        </div>
       ))}
     </section>
   );
